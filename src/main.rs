ab1beab6d58568bec123e0576b91045065f64355
viniciusfonseca <vfonseca1618@gmail.com>
2023-08-17 10:47:40 -0300

    add gist TRGM

--- src/main.rs
+++ src/main.rs
@@ -145,11 +145,13 @@ struct ParametrosBusca {
 #[actix_web::get("/pessoas")]
 async fn buscar_pessoas(parametros: web::Query<ParametrosBusca>, pool: web::Data<Pool>) -> APIResult {
 
-    let t = format!("{}", parametros.t.replace(' ', "*"));
+    // let t = format!("{}:*", parametros.t.replace(' ', "*"));
+    let t = format!("%{}%", parametros.t.to_lowercase().replace(' ', "*"));
     let result = {
         let conn = pool.get().await?;
         let rows = conn.query(
-            "SELECT ID, APELIDO, NOME, NASCIMENTO, STACK FROM PESSOAS P WHERE TO_TSQUERY('BUSCA', $1) @@ BUSCA LIMIT 50;", &[&t]
+            // "SELECT ID, APELIDO, NOME, NASCIMENTO, STACK FROM PESSOAS P WHERE TO_TSQUERY('BUSCA', $1) @@ BUSCA LIMIT 50;", &[&t]
+            "SELECT ID, APELIDO, NOME, NASCIMENTO, STACK FROM PESSOAS P WHERE P.BUSCA_TRGM LIKE $1 LIMIT 50;", &[&t]
         ).await?;
         rows.iter().map(|row| PessoaDTO::from(row)).collect::<Vec<PessoaDTO>>()
     };
@@ -253,7 +255,7 @@ async fn main() -> AsyncVoidResult {
         {
             let http_client = reqwest::Client::new();
             let nginx_url = "http://172.17.0.1:9999/pessoas";
-            let mount_body = |n: u16| format!("{{\"apelido\":\"vaf{n}\",\"nascimento\":\"1999-01-01\",\"nome\":\"VAF\"}}");
+            let mount_body = |n: u16| format!("{{\"apelido\":\"WARMUP::vaf{n}\",\"nascimento\":\"1999-01-01\",\"nome\":\"VAF\"}}");
             let mut f = vec![];
             let v = vec![0, 1, 2, 1, 0];
             for i in 0..511 {
@@ -265,10 +267,14 @@ async fn main() -> AsyncVoidResult {
                 }
             }
             futures::future::join_all(f).await;
-            pool_async.get().await.unwrap().execute("DELETE FROM PESSOAS;", &[]).await.unwrap();
+            let pool_async_async = pool_async.clone();
+            tokio::spawn(async move {
+                tokio::time::sleep(Duration::from_secs(2)).await;
+                pool_async_async.get().await.unwrap().execute("DELETE FROM PESSOAS WHERE APELIDO LIKE 'WARMUP%';", &[]).await.unwrap();
+            });
         }
         loop {
-            tokio::time::sleep(Duration::from_secs(5)).await;
+            tokio::time::sleep(Duration::from_secs(2)).await;
             let queue = queue_async.clone();
             if queue.len() == 0 { continue }
             batch_insert(pool_async.clone(), queue).await;
